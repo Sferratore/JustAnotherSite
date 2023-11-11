bb3efba6f32c343cb16fa7e24328dbae70022d47
Sferratore <sferratore.francescopaolo00@gmail.com>
2023-11-11 16:34:43 +0100

    Just completed another site...nothing to look at...really...normal CRUD operations...

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,5 +8,14 @@ app = Flask(__name__)
 def index():
     return "All my FELLAS!!"
 
+@app.route('/summon')
+def summon_meme():
+    url = "https://meme.breakingbranches.tech/api?limit=1&type=dank"
+    #requests.request("GET", url) sends a GET request to the specified URL.
+    #.text extracts the text content from the response.
+    #json.loads(...) uses the json module to load (parse) the JSON-formatted text into a Python data structure. The loads function is used for parsing a JSON string.
+    response = json.loads(requests.request("GET", url).text)
+    meme = response["memes"][0]
+    return render_template("index.html", meme_title = meme["title"], meme_url = meme["url"], meme_author = meme["author"])
 
-app.run()
+app.run()  
